0215bc59239b0c930fe5241ce75096995e4a2d99
Bruno-Dotto <brunodtt@gmail.com>
2026-07-01 21:40:20 -0300

    feat: configure CI/CD with GitHub Actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,33 @@
 const { defineConfig } = require("cypress");
+const { plugin: cypressGrepPlugin } = require("@cypress/grep/plugin");
 
 module.exports = defineConfig({
+
+  reporter: "mochawesome",
+
+  reporterOptions: {
+
+    reportDir: "cypress/reports",
+
+    overwrite: false,
+
+    html: false,
+
+    json: true,
+
+    quiet: true
+
+  },
+  
   allowCypressEnv: false,
 
   e2e: {
     baseUrl: "https://restful-booker.herokuapp.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      cypressGrepPlugin(config);
+
+      return config;
     },
   },
 });
